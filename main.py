947b3e6e1cde030fc95266d1d77d6661204251c1
mustafauskuplu <mu.uskuplu@gmail.com>
2025-04-19 20:52:26 +0200

    Limit FPS to 60

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +20,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
